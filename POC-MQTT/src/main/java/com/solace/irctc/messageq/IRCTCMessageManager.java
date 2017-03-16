298bb115823f096cc31c6c449933d11849047e58
ArghyaSanyal <sanyal.arghya@gmail.com>
2017-03-16 10:34:41 +0530

    Some changes

--- POC-MQTT/src/main/java/com/solace/irctc/messageq/IRCTCMessageManager.java
+++ POC-MQTT/src/main/java/com/solace/irctc/messageq/IRCTCMessageManager.java
@@ -34,93 +34,7 @@ public class IRCTCMessageManager {
     	consumeMessage();
     }
     
-    public static void consumeMQTTMessage() throws JCSMPException, InterruptedException
-    {
-    	 final List <String> outputStr= new ArrayList<String>();
-    	try {
-            // Create an Mqtt client
-
-            MqttAsyncClient mqttClient = new MqttAsyncClient("", "HelloWorldQoS1Consumer");
-            MqttConnectOptions connOpts = new MqttConnectOptions();
-         
-            connOpts.setUserName("sample");
-            connOpts.setPassword("sample".toCharArray());
-            connOpts.setCleanSession(true);
-            // Connect the client
-            System.out.println("Connecting to Solace broker: tcp://");
-            IMqttToken conToken = mqttClient.connect(connOpts);
-            conToken.waitForCompletion(10000);
-            if (!conToken.isComplete() || conToken.getException() != null) {
-                System.out.println("Error connecting: " + conToken.getException());
-                System.exit(-1);
-            }
-            System.out.println("Connected");
-
-            // Latch used for synchronizing b/w threads
-            final CountDownLatch latch = new CountDownLatch(1);
-
-            // Callback - Anonymous inner-class for receiving messages
-            mqttClient.setCallback(new MqttCallback() {
-
-                public void messageArrived(String topic, MqttMessage message) throws Exception {
-                    // Called when a message arrives from the server that
-                    // matches any subscription made by the client
-                    String time = new Timestamp(System.currentTimeMillis()).toString();
-                    System.out.println("\nReceived a Message!" +
-                            "\n\tTime:    " + time + 
-                            "\n\tTopic:   " + topic + 
-                            "\n\tMessage: " + new String(message.getPayload()) + 
-                            "\n\tQoS:     " + message.getQos() + "\n");
-                    String outputchr = new String(message.getPayload()) ;
-                    outputStr.add(0, outputchr);
-                    latch.countDown(); // unblock main thread
-                }
-
-                public void connectionLost(Throwable cause) {
-                    System.out.println("Connection to Solace broker lost!" + cause.getMessage());
-                    latch.countDown();
-                }
-
-                public void deliveryComplete(IMqttDeliveryToken token) {
-                }
-
-            });
-
-            // Topic filter the client will subscribe to
-            final String subTopic = "T/Booking/request";
-            // Subscribe client to the topic filter with QoS level of 1
-            System.out.println("Subscribing client to topic: " + subTopic);
-            IMqttToken subToken = mqttClient.subscribe(subTopic, 1);
-            subToken.waitForCompletion(10000);
-            if (!subToken.isComplete() || subToken.getException() != null) {
-                System.out.println("Error subscribing: " + subToken.getException());
-                System.exit(-1);
-            }
-            if (subToken.getGrantedQos()[0] != 1) {
-                System.out.println("Expected OoS level 1 but got OoS level: " + subToken.getGrantedQos()[0]);
-                System.exit(-1);
-            }
-            System.out.println("Subscribed with OoS level 1 and waiting to receive msgs");
-
-            // Wait for the message to be received
-            try {
-                latch.await(); // block here until message received, and latch will flip
-            } catch (InterruptedException e) {
-                System.out.println("I was awoken while waiting");
-            }
-            if(outputStr !=null && outputStr.size()>0)
-                   publishMessage(outputStr.get(0));
- 
-
-        } catch (MqttException me) {
-            System.out.println("reason " + me.getReasonCode());
-            System.out.println("msg " + me.getMessage());
-            System.out.println("loc " + me.getLocalizedMessage());
-            System.out.println("cause " + me.getCause());
-            System.out.println("excep " + me);
-            me.printStackTrace();
-        }
-    }
+  
     public static void consumeMessage() throws JCSMPException, InterruptedException
     {
         final List <String> outputStr= new ArrayList<String>();
@@ -128,7 +42,7 @@ public class IRCTCMessageManager {
         System.out.println("QueueConsumer initializing...");
         // Create a JCSMP Session
         final JCSMPProperties properties = new JCSMPProperties();
-        properties.setProperty(JCSMPProperties.HOST, ""); // msg-backbone ip:port
+        properties.setProperty(JCSMPProperties.HOST, "35.156.24.107"); // msg-backbone ip:port
         properties.setProperty(JCSMPProperties.VPN_NAME, "Sample-Message-VPM"); // message-vpn
         properties.setProperty(JCSMPProperties.USERNAME, "sample"); // client-username
         properties.setProperty(JCSMPProperties.PASSWORD, "sample"); // 
@@ -216,13 +130,13 @@ public class IRCTCMessageManager {
         
         try {
             // Create an Mqtt client
-            MqttClient mqttClient = new MqttClient("tcp://", "HelloWorldQoS1Producer");
+            MqttClient mqttClient = new MqttClient("tcp://35.156.24.107:8001", "HelloWorldQoS1Producer");
             MqttConnectOptions connOpts = new MqttConnectOptions();
             connOpts.setUserName("sample");
             connOpts.setPassword("sample".toCharArray());
             connOpts.setCleanSession(true);
             // Connect the client
-            System.out.println("Connecting to Solace broker: tcp://" + "tcp://");
+            System.out.println("Connecting to Solace broker: tcp://" + "tcp://35.156.24.107:8001");
             mqttClient.connect(connOpts);
             System.out.println("Connected");
             // Create a Mqtt message
